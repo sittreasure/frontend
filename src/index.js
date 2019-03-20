e7ee65756495dc66983bc35f9a149988a8473c59
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-20 14:38:07 +0700

    [UPDATE] use style sheet from local & react-reflex

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'react-reflex/styles.css'
 
 import App from './App'
+import './static/styles.css'
 
 ReactDOM.render(
   <App />,
