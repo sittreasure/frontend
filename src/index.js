f0752dee4bce510b21d715788b7e41e6c36db21f
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-22 21:16:24 +0700

    [UPDATE] integrate redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import reduxStore from './redux'
 import './static/css/styles.css'
 
+const store = reduxStore()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 )
