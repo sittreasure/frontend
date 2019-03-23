e75d7e0f5542e875a44e0fd798da38813e37530a
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-23 12:51:32 +0700

    [UPDATE] add tab info & folder

--- src/features/playground/index.js
+++ src/features/playground/index.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-import { SideTab } from '../../utilities'
+import { SideTab, Tab } from '../../utilities'
 // import Directory from './Directory'
 
-const Container = styled.div`
-  background-color: #272727;
+const Wrappper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
   width: 100%;
   height: 100%;
+  background-color: #272727;
+  overflow: hidden;
+`
+
+const Container = styled.div`
+  padding: 0px 9px;
+`
+
+const Icon = styled.img`
+  width: 26px;
+  height: 26px;
 `
 
 class Playground extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      directory: false,
+    }
+  }
+
+  toggleDirectory(event) {
+    event.preventDefault()
+    this.setState({
+      directory: !this.state.directory,
+    })
+  }
+
   render() {
     return (
-      <Container>
-        <SideTab />
-      </Container>
+      <Wrappper>
+        <SideTab>
+          <Tab>
+            <Icon src={require('../../assets/images/info.png')} />
+          </Tab>
+          <Tab onClick={e => this.toggleDirectory(e)}>
+            <Icon src={require('../../assets/images/folder.png')} />
+          </Tab>
+        </SideTab>
+        <Container />
+      </Wrappper>
     )
   }
 }
