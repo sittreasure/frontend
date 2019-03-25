2fc5d50e7e4a80e600bd687461cab134ff425cf6
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-25 23:28:47 +0700

    [UPDATE] destruct duplicate styled & function

--- src/features/playground/LabelGroup.js
+++ src/features/playground/LabelGroup.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
+import { Styled, functions } from './utilities'
+
 const Container = styled.div`
   width: 100%;
   height: ${props => props.show ? props.height : '28px'};
@@ -12,6 +14,7 @@ const Container = styled.div`
 const TitleWrapper = styled.div`
   display: flex;
   cursor: pointer;
+  height: 28px;
 `
 
 const ArrowWrapper = styled.div`
@@ -27,15 +30,6 @@ const Arrow = styled.img`
   transform: rotate(${props => props.show ? 90 : 0 }deg);
 `
 
-const Title = styled.div`
-  font-family: "ThaiSans Neue";
-  font-style: normal;
-  font-weight: bold;
-  font-size: 21px;
-  line-height: normal;
-  color: #C4C4C4;
-`
-
 const ContentContainer = styled.div`
   padding-top: 5px;
 `
@@ -48,17 +42,14 @@ class LabelGroup extends Component {
     }
   }
 
-  toggleShow(e) {
-    e.preventDefault()
-    this.setState({
-      show: !this.state.show,
-    })
-  }
-
   render() {
     return (
-      <Container show={this.state.show} height={this.props.height}>
-        <TitleWrapper onClick={e => this.toggleShow(e)}>
+      <Container
+        show={this.state.show}
+        height={this.props.height}
+        className="transition"
+      >
+        <TitleWrapper onClick={e => functions.toggleShow(e, this)}>
           <ArrowWrapper>
             <Arrow
               src={require('../../assets/images/next.png')}
@@ -66,7 +57,7 @@ class LabelGroup extends Component {
               className="transition"
             />
           </ArrowWrapper>
-          <Title>{this.props.title}</Title>
+          <Styled.Title>{this.props.title}</Styled.Title>
         </TitleWrapper>
         <ContentContainer>
           {this.props.children}
