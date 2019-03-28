efafbb182725f7c150418fa8baf780498215ef0a
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-28 22:35:59 +0700

    [UPDATE] add directory data

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,11 @@
 import { all } from 'redux-saga/effects'
 
 import directoryMetadata from './directoryMetadata'
+import directoryData from './directoryData'
 
 export default function* () {
   yield all([
     directoryMetadata(),
+    directoryData(),
   ])
 }
