6d6b2392bf2fd6aa5f782eece610d85e51113976
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-28 22:35:05 +0700

    [ADD] create saga function fetch file api & dispatch set data

---
+++ src/sagas/directoryData.js
@@ -0,0 +1,25 @@
+import { call, put, fork, takeEvery } from 'redux-saga/effects'
+
+import axios from '../libs/axios'
+import DirectoryActions, { DirectoryTypes } from '../redux/directoryStore'
+
+function* getData({ id = '' }) {
+  try {
+    let objectData
+    yield call(async () => {
+      const { data } = await axios.get(`minios/file?object_name=${id}`)
+      objectData = data
+    })
+    yield put(DirectoryActions.setData(id, objectData))
+  } catch (error) {
+    console.log('>>> [directoryData.js:15] error : ', error)
+  }
+}
+
+function* getDataListener() {
+  yield takeEvery(DirectoryTypes.GET_DATA, getData)
+}
+
+export default function* datasaga() {
+  yield fork(getDataListener)
+}
