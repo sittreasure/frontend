0e52ade28bb7684f2da343c848b122c5063d5ef6
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-29 10:04:35 +0700

    [UPDATE] compose middleware

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const create = (rootReducer, rootSaga) => {
   const sagaMiddleware = createSagaMiddleware()
   const middleware = applyMiddleware(sagaMiddleware)
+  const composeDevTools = composeWithDevTools(middleware)
   const store = createStore(
     rootReducer,
-    middleware
+    composeDevTools
   )
   sagaMiddleware.run(rootSaga)
   return store
