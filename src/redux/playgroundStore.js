776740602e74286925c7d966fbc5d77bad30b8c6
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-29 23:36:13 +0700

    [UPDATE] store for feature playground with directory & open

---
+++ src/redux/playgroundStore.js
@@ -0,0 +1,36 @@
+import { createActions, createReducer } from 'reduxsauce'
+import Immutable from 'seamless-immutable'
+
+const { Types, Creators } = createActions({
+  toggleDirectory: null,
+  setOpen: ['id'],
+})
+
+export const PlaygroundTypes = Types
+export default Creators
+
+const INITIAL_STATE = Immutable({
+  directory: false,
+  open: {
+    id: null,
+    name: null,
+  },
+})
+
+const toggleDirectory = (state = INITIAL_STATE) => ({
+  ...state,
+  directory: !state.directory,
+})
+
+const setOpen = (state = INITIAL_STATE, { id }) => ({
+  ...state,
+  open: {
+    id: id,
+    name: id.split('/').pop(),
+  },
+})
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.SET_OPEN]: setOpen,
+  [Types.TOGGLE_DIRECTORY]: toggleDirectory,
+})
