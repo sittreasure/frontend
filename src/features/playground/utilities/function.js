927c7c92a09782eec0ebcc7f7e967fc055268b37
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-03-30 00:27:51 +0700

    [UPDATE] add function show icon for get icom image src

--- src/features/playground/utilities/function.js
+++ src/features/playground/utilities/function.js
@@ -5,8 +5,22 @@ const toggleShow = (event, component) => {
   })
 }
 
+const showIcon = type => {
+  switch (type) {
+    case 'java':
+      return require('../../../assets/images/java.png')
+    case 'html':
+      return require('../../../assets/images/html5.png')
+    case 'xml':
+      return require('../../../assets/images/xml.png')
+    default:
+      return require('../../../assets/images/info.png')
+  }
+}
+
 const functions = {
   toggleShow,
+  showIcon,
 }
 
 export default functions
