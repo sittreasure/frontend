f7fb376c8fa03971c14d9d50e8045c00841dffc0
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-13 21:44:21 +0700

    [UPDATE] import playground check compile

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,11 +3,13 @@ import { all } from 'redux-saga/effects'
 import directoryMetadata from './directoryMetadata'
 import directoryData from './directoryData'
 import playgroundCompile from './playgroundCompile'
+import playgroundCheckCompile from './playgroundCheckCompile'
 
 export default function* () {
   yield all([
     directoryMetadata(),
     directoryData(),
     playgroundCompile(),
+    playgroundCheckCompile(),
   ])
 }
