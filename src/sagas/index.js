711e0607f88dfd1fb3bf26b4040b6b6fc6516909
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-25 21:49:29 +0700

    [UPDATE] call remove data saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,6 +2,7 @@ import { all } from 'redux-saga/effects'
 
 import directoryMetadata from './directoryMetadata'
 import directoryData from './directoryData'
+import directoryRemoveData from './directoryRemoveData'
 import directoryFileType from './directoryFileType'
 import playgroundCompile from './playgroundCompile'
 import playgroundCheckCompile from './playgroundCheckCompile'
@@ -12,6 +13,7 @@ export default function* () {
   yield all([
     directoryMetadata(),
     directoryData(),
+    directoryRemoveData(),
     directoryFileType(),
     playgroundCompile(),
     playgroundCheckCompile(),
