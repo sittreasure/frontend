2cc5ed20f4475e392a65467243352e97f42e5c43
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-04-25 16:40:07 +0700

    [UPDATE] move label group to directory

--- src/features/playground/Directory.js
+++ src/features/playground/Directory.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 
-import LabelGroup from './LabelGroup'
 import Label from './Label'
 import LabelFolder from './LabelFolder'
 import DirectoryActions from '../../redux/directoryStore'
+import { Styled, functions } from './utilities'
 
 const DirectoryContainer = styled.div`
   display: flex;
@@ -32,7 +32,44 @@ const Title = styled.div`
   cursor: default;
 `
 
+const GroupContainer = styled.div`
+  width: 100%;
+  height: ${props => props.show ? '100%' : '28px'};
+  margin-top: 2px;
+  overflow: hidden;
+`
+
+const TitleWrapper = styled.div`
+  display: flex;
+  cursor: pointer;
+  height: 28px;
+`
+
+const ArrowWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 42px;
+`
+
+const Arrow = styled.img`
+  width: 6px;
+  height: 12px;
+  transform: rotate(${props => props.show ? 90 : 0 }deg);
+`
+
+const ContentContainer = styled.div`
+  padding-top: 5px;
+`
+
 class Directory extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: true,
+    }
+  }
+
   componentDidMount() {
     this.props.dispatch(DirectoryActions.getMetadata('playground/'))
     this.props.dispatch(DirectoryActions.getFileType())
@@ -80,15 +117,29 @@ class Directory extends Component {
     return (
       <DirectoryContainer show={this.props.show} className="transition">
         <Title>File Editor</Title>
-        <LabelGroup title="Open Tabs" height="30%" />
-        <LabelGroup title="Files" height="100%">
-          {this.props.directory.map(data => {
-            if(data.isDir) {
-              return this.showLabelFolder(data)
-            }
-            return this.showLabel(data)
-          })}
-        </LabelGroup>
+        <GroupContainer
+          show={this.state.show}
+          className="transition"
+        >
+          <TitleWrapper onClick={e => functions.toggleShow(e, this)}>
+            <ArrowWrapper>
+              <Arrow
+                src={require('../../assets/images/next.png')}
+                show={this.state.show}
+                className="transition"
+              />
+            </ArrowWrapper>
+            <Styled.Title>Directory</Styled.Title>
+          </TitleWrapper>
+          <ContentContainer>
+            {this.props.directory.map(data => {
+              if(data.isDir) {
+                return this.showLabelFolder(data)
+              }
+              return this.showLabel(data)
+            })}
+          </ContentContainer>
+        </GroupContainer>
       </DirectoryContainer>
     )
   }
