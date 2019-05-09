d9eb3df78b61a364be92371e4ae8bcc83aa2b722
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-05-09 12:51:10 +0700

    [UPDATE] create function cut & paste file

--- src/features/playground/ContextMenu.js
+++ src/features/playground/ContextMenu.js
@@ -36,7 +36,8 @@ const Option = styled(Styled.Title)`
   padding-left: 50px;
   padding-top: 4px;
   padding-bottom: 4px;
-  cursor: pointer;
+  cursor: ${props => props.disable ? 'not-allowed' : 'pointer'};
+  opacity: ${props => props.disable ? 0.5 : 1};
 `
 
 class ContextMenu extends Component {
@@ -57,6 +58,29 @@ class ContextMenu extends Component {
     this.props.dispatch(DirectoryActions.toggleContextMenuNewFile())
   }
 
+  cutFile(event) {
+    const { isDir } = this.props.contextMenu
+    if (!isDir) {
+      this.closeContextMenu(event)
+      this.props.dispatch(DirectoryActions.setContextMenuCut(this.props.contextMenu.id))
+    }
+  }
+
+  pasteFile(event) {
+    const { contextMenu: { cut }, directory } = this.props
+    if (cut) {
+      this.closeContextMenu(event)
+      const paste = this.changeId()
+      const file = { ...this.findFile(directory, cut) }
+      const remove = file.id
+      file.id = paste + file.name
+      file.save = false
+      this.props.dispatch(DirectoryActions.addFile(paste, file))
+      this.props.dispatch(DirectoryActions.setContextMenuCut(null))
+      this.props.dispatch(DirectoryActions.removeData(remove))
+    }
+  }
+
   openRemove(event) {
     this.closeContextMenu(event)
     this.props.dispatch(DirectoryActions.toggleContextMenuRemove())
@@ -70,10 +94,25 @@ class ContextMenu extends Component {
       id = id.join('/')
     }
     this.props.dispatch(DirectoryActions.setContextMenuId(id))
+    return id
+  }
+
+  findFile(parents, id) {
+    for (let i = 0; i < parents.length; i++) {
+      const child = parents[i]
+      if (child.id === id) {
+        return child
+      }
+      else {
+        if (child.isDir && id.search(child.id) !== -1) {
+          return this.findFile(child.data, id)
+        }
+      }
+    }
   }
 
   render() {
-    const { show, x, y, overflow, isDir } = this.props.contextMenu
+    const { show, x, y, overflow, isDir, cut } = this.props.contextMenu
     return (
       <Fragment>
         <Background
@@ -88,6 +127,18 @@ class ContextMenu extends Component {
         >
           <Option onClick={e => this.openNewFolder(e)}>New Folder</Option>
           <Option onClick={e => this.openNewFile(e)}>New File</Option>
+          <Option
+            onClick={e => this.cutFile(e)}
+            disable={isDir}
+          >
+            Cut
+          </Option>
+          <Option
+            onClick={e => this.pasteFile(e)}
+            disable={!cut}
+          >
+            Paste
+          </Option>
           {!isDir
             ? (
               <Option onClick={e => this.openRemove(e)}>Remove</Option>
@@ -103,10 +154,12 @@ class ContextMenu extends Component {
 ContextMenu.propTypes = {
   dispatch: PropTypes.func,
   contextMenu: PropTypes.object,
+  directory: PropTypes.array,
 }
 
 const mapStateToProps = state => ({
   contextMenu: state.directoryStore.contextMenu,
+  directory: state.directoryStore.directory,
 })
 
 export default connect(mapStateToProps)(ContextMenu)
