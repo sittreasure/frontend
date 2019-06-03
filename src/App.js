576e203c1d096bcf27d2f9f562c96c52940fad71
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-06-03 15:24:08 +0700

    [UPDATE] add route /login & change / redirect from /playground to /login

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 import Playground from './features/playground'
+import Login from './features/login'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path='/' exact render={() => <Redirect to='/playground' />} />
+        <Route path='/' exact render={() => <Redirect to='/login' />} />
         <Route path='/playground' exact component={Playground} />
+        <Route path='/login' exact component={Login} />
       </Router>
     )
   }
