e0cd5d9bd343af940c106082fa5ebcfeb81d7270
Boat <boat46450@hotmail.com>
2019-08-05 15:30:18 +0700

    :sparkles: add auth header

--- src/sagas/directoryFileType.js
+++ src/sagas/directoryFileType.js
@@ -1,13 +1,21 @@
 import { call, put, fork, takeEvery } from 'redux-saga/effects'
 
 import axios from '../libs/axios'
+import { accessToken } from '../utils'
 import DirectoryActions, { DirectoryTypes } from '../redux/directoryStore'
 
 function* getFileType() {
   try {
     let fileType
     yield call(async () => {
-      const { data } = await axios.get('/fileapi/v1/file/')
+      const { data } = await axios.get(
+        '/fileapi/v1/file/',
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken.getToken()}`,
+          },
+        }
+      )
       fileType = data
     })
     yield put(DirectoryActions.setFileType(fileType))
--- src/sagas/directoryMetadata.js
+++ src/sagas/directoryMetadata.js
@@ -1,18 +1,26 @@
 import { call, put, fork, takeEvery } from 'redux-saga/effects'
 
 import axios from '../libs/axios'
+import { accessToken } from '../utils'
 import DirectoryActions, { DirectoryTypes } from '../redux/directoryStore'
 
 function* getMetadata({ prefix = '' }) {
   try {
     let metadata
     yield call(async () => {
-      const { data } = await axios.get(`/fileapi/v1/minios/file?prefix_name=${prefix}`)
+      const { data } = await axios.get('/fileapi/v1/minios/file/', {
+        params: {
+          prefix_name: prefix,
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken.getToken()}`,
+        },
+      })
       metadata = data
     })
     yield put(DirectoryActions.setMetadata(metadata))
   } catch (error) {
-    console.log('>>> [directoryMetadata.js:15] error : ', error)
+    console.log('>>> [directoryMetadata.js:23] error : ', error)
   }
 }
 
