63ccc9109610132bafc42b07c276ad44eeffab90
Boat <boat46450@hotmail.com>
2019-08-05 15:29:41 +0700

    :sparkles: import user me saga & fix path of user login facebook

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects'
 
-import loginFacebook from './loginFacebook'
+import userLoginFacebook from './userLoginFacebook'
+import userMe from './userMe'
 import directoryMetadata from './directoryMetadata'
 import directoryData from './directoryData'
 import directoryRemoveData from './directoryRemoveData'
@@ -10,9 +11,10 @@ import playgroundCheckCompile from './playgroundCheckCompile'
 import playgroundCompileLog from './playgroundCompileLog'
 import playgroundSave from './playgroundSave'
 
-export default function* () {
+export default function*() {
   yield all([
-    loginFacebook(),
+    userLoginFacebook(),
+    userMe(),
     directoryMetadata(),
     directoryData(),
     directoryRemoveData(),
