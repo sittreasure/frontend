03f05692ef2d5c04537a907133475dc66dcef544
Boat <boat46450@hotmail.com>
2019-08-05 15:23:23 +0700

    :fire: move to user store

--- src/redux/loginStore.js
+++
@@ -1,8 +0,0 @@
-import { createActions } from 'reduxsauce'
-
-const { Types, Creators } = createActions({
-  loginFacebook: ['id', 'name', 'surname', 'image'],
-})
-
-export const LoginTypes = Types
-export default Creators
