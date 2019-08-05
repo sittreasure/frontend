facc74de406249e79990fef21fe497b687b8ed16
Boat <boat46450@hotmail.com>
2019-08-05 15:20:30 +0700

    :zap: using root path for login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Playground from './features/playground'
-import Login from './features/login'
+import Login from './features'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path='/' exact render={() => <Redirect to='/login' />} />
+        <Route path='/' exact component={Login} />
         <Route path='/playground' exact component={Playground} />
-        <Route path='/login' exact component={Login} />
       </Router>
     )
   }
