4865bffba61c1fdc573fc2929c151bc9bd8074ce
Boat <boat46450@hotmail.com>
2019-08-05 15:28:23 +0700

    :sparkles: add user store reducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -7,6 +7,7 @@ const createRedux = () => {
   const rootReducer = combineReducers({
     directoryStore: require('./directoryStore').reducer,
     playgroundStore: require('./playgroundStore').reducer,
+    userStore: require('./userStore').reducer,
   })
 
   return createStore(rootReducer, rootSaga)
