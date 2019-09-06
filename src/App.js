0ebd037a53e010d815fffd01ed5034072764cc77
Boat <boat46450@hotmail.com>
2019-09-06 12:17:27 +0700

    :sparkles: add route home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Playground from './features/playground'
 import Login from './features'
+import Home from './features/home'
+import Playground from './features/playground'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path='/' exact component={Login} />
-        <Route path='/playground' exact component={Playground} />
+        <Route path="/" exact component={Login} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/playground" exact component={Playground} />
       </Router>
     )
   }
