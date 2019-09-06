c8877b4e9af1865fdd8b3ac7d079635166ad52b4
Boat <boat46450@hotmail.com>
2019-09-06 12:27:20 +0700

    :sparkles: add function remove token

--- src/utils/accessToken.js
+++ src/utils/accessToken.js
@@ -1,7 +1,11 @@
-const getToken = () => localStorage.getItem('token')
-const setToken = token => localStorage.setItem('token', token)
+const key = 'token'
+
+const getToken = () => localStorage.getItem(key)
+const setToken = token => localStorage.setItem(key, token)
+const removeToken = () => localStorage.removeItem(key)
 
 export default {
   getToken,
   setToken,
+  removeToken,
 }
