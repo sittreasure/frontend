e58298b1153018985b02bbe4cc03fb06e11309ac
Boat <boat46450@hotmail.com>
2019-09-06 12:28:14 +0700

    :sparkles: add route logout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from './features'
 import Home from './features/home'
 import Playground from './features/playground'
-
+import Logout from './features/logout'
 class App extends Component {
   render() {
     return (
@@ -12,6 +12,7 @@ class App extends Component {
         <Route path="/" exact component={Login} />
         <Route path="/home" exact component={Home} />
         <Route path="/playground" exact component={Playground} />
+        <Route path="/logout" exact component={Logout} />
       </Router>
     )
   }
