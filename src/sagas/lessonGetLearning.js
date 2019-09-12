294508cb1dc51c640a872f0a332e690bf5a36639
Boat <boat46450@hotmail.com>
2019-09-12 14:50:58 +0700

    :sparkles: saga for call api learning and format data

---
+++ src/sagas/lessonGetLearning.js
@@ -0,0 +1,32 @@
+import { call, put, fork, takeEvery } from 'redux-saga/effects'
+
+import axios from '../libs/axios'
+import { accessToken } from '../utils'
+import LessonActions, { LessonTypes } from '../redux/lessonStore'
+
+function* getLessonLearning() {
+  try {
+    let learnings
+    yield call(async () => {
+      const { data } = await axios.get('/mainapi/v1/lessons/learning/', {
+        headers: {
+          Authorization: `Bearer ${accessToken.getToken()}`,
+        },
+      })
+      learnings = data
+    })
+    const learned = []
+    learnings.map(learning => learned.push(learning.lesson))
+    yield put(LessonActions.setLessonLearning(learned))
+  } catch (error) {
+    console.log('>>> [lessonGetLearning.js:22] error : ', error)
+  }
+}
+
+function* getLessonLearningListener() {
+  yield takeEvery(LessonTypes.GET_LESSON_LEARNING, getLessonLearning)
+}
+
+export default function* lessonlearningsaga() {
+  yield fork(getLessonLearningListener)
+}
