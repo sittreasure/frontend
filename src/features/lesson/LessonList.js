5fa5274702d152fd0c1082517d14185635889b5b
Boat <boat46450@hotmail.com>
2019-09-14 14:08:59 +0700

    :sparkles: add funtion render lesson label

--- src/features/lesson/LessonList.js
+++ src/features/lesson/LessonList.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
+
+import Group from './Group'
 
 const Container = styled.div`
   display: flex;
@@ -21,16 +24,66 @@ const Title = styled.div`
   font-size: 23px;
   line-height: normal;
   color: #61d0ff;
-  margin-top: 12px;
-  margin-left: 17px;
+  margin: 12px 0 0 17px;
   cursor: default;
 `
 
 class LessonList extends Component {
+  renderGroup() {
+    const groups = []
+    if (this.props.currentLesson) {
+      const currentLesson = this.props.lessons.filter(lesson => {
+        return lesson.id === this.props.currentLesson
+      })[0]
+      if (this.props.lessons.length !== 0) {
+        this.props.group.map(group => {
+          const lessons = this.props.lessons.filter(lesson => {
+            return lesson.lessonGroup === group.id
+          })
+          const temp = {
+            lessons,
+            groupId: lessons[0].lessonGroup,
+          }
+          lessons.reduce((accumulator, currentValue) => {
+            const learned = this.props.learned.indexOf(currentValue.id)
+            accumulator = accumulator || learned !== -1
+            temp.show = accumulator
+          }, false)
+
+          temp.hasNaxtLesson =
+            lessons.filter(lesson => {
+              return lesson.index === currentLesson.index + 1
+            }).length !== 0
+          groups.push(temp)
+        })
+      }
+      return groups.map((group, index) => {
+        const titleIndex = this.props.group.findIndex(
+          g => g.id === group.groupId
+        )
+        const title = this.props.group[titleIndex].name
+        if (!(group.show || group.hasNaxtLesson)) {
+          return null
+        }
+        return (
+          <Group
+            title={title}
+            lessons={group.lessons}
+            currentLesson={this.props.currentLesson}
+            nextLessonIndex={currentLesson.index + 1}
+            show={group.show}
+            key={index}
+          />
+        )
+      })
+    }
+  }
+
   render() {
     return (
       <Container show={this.props.show} className="transition">
         <Title>All Lessons</Title>
+        {this.renderGroup()}
       </Container>
     )
   }
@@ -38,6 +91,18 @@ class LessonList extends Component {
 
 LessonList.propTypes = {
   show: PropTypes.bool,
+  group: PropTypes.array,
+  lessons: PropTypes.array,
+  learned: PropTypes.array,
+  currentLesson: PropTypes.number,
 }
 
-export default LessonList
+const mapStateToProps = state => ({
+  show: state.lessonStore.openList,
+  group: state.lessonStore.group,
+  lessons: state.lessonStore.lessons,
+  learned: state.lessonStore.learned,
+  currentLesson: state.lessonStore.currentLesson,
+})
+
+export default connect(mapStateToProps)(LessonList)
