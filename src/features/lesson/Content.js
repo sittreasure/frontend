6a36ca8c7533e4c8c5e8040ff76fff52a390493e
Boat <boat46450@hotmail.com>
2019-09-17 11:25:09 +0700

    :sparkles: use real data from database & show group name

--- src/features/lesson/Content.js
+++ src/features/lesson/Content.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
 import ReactMarkdown from 'react-markdown'
 
 import { ReactComponent as SkipIcon } from '../../assets/images/skip.svg'
@@ -79,51 +81,20 @@ const NextButton = styled(Button)`
 
 class Content extends Component {
   render() {
+    const lesson = this.props.lessons.filter(lesson => {
+      return lesson.id === this.props.currentLesson
+    })[0]
+    if (lesson === undefined) {
+      return null
+    }
+    const group = this.props.group.filter(group => {
+      return group.id === lesson.lessonGroup
+    })[0]
     return (
       <Container>
-        <Title>Lesson 1</Title>
+        <Title>{`${group && group.name} - ${lesson.topic}`}</Title>
         <MarkdownContainer>
-          <ReactMarkdown
-            source="- `source` or `children` - _string_ The Markdown source to parse (**required**)
-- `className` - _string_ Class name of the container element. If none is passed, a container will not be rendered.
-- `escapeHtml` - _boolean_ Setting to `false` will cause HTML to be rendered (see notes below about proper HTML support). Be aware that setting this to `false` might cause security issues if the
-  input is user-generated. Use at your own risk. (default: `true`).
-- `skipHtml` - _boolean_ Setting to `true` will skip inlined and blocks of HTML (default: `false`).
-- `sourcePos` - _boolean_ Setting to `true` will add `data-sourcepos` attributes to all elements,
-  indicating where in the markdown source they were rendered from (default: `false`).
-- `rawSourcePos` - _boolean_ Setting to `true` will pass a `sourcePosition` property to all renderers with structured source position information (default: `false`).
-- `includeNodeIndex` - _boolean_ Setting to `true` will pass `index` and `parentChildCount` props to all renderers (default: `false`).
-- `allowedTypes` - _array_ Defines which types of nodes should be allowed (rendered). (default: all
-  types).
-- `disallowedTypes` - _array_ Defines which types of nodes should be disallowed (not rendered).
-  (default: none).
-- `unwrapDisallowed` - _boolean_ Setting to `true` will try to extract/unwrap the children of
-  disallowed nodes. For instance, if disallowing `Strong`, the default behaviour is to simply skip
-  the text within the strong altogether, while the behaviour some might want is to simply have the
-  text returned without the strong wrapping it. (default: `false`)
-- `allowNode` - _function_ Function execute if in order to determine if the node should be allowed.
-  Ran prior to checking `allowedTypes`/`disallowedTypes`. Returning a truthy value will allow the
-  node to be included. Note that if this function returns `true` and the type is not in
-  `allowedTypes` (or specified as a `disallowedType`), it won't be included. The function will
-  receive three arguments argument (`node`, `index`, `parent`), where `node` contains different
-  properties depending on the node type.
-- `linkTarget` - _function|string_ Sets the default target attribute for links. If a function is
-  provided, it will be called with `url`, `text`, and `title` and should return a string
-  (e.g. `_blank` for a new tab). Default is `undefined` (no target attribute).
-- `transformLinkUri` - _function|null_ Function that gets called for each encountered link with a
-  single argument - `uri`. The returned value is used in place of the original. The default link URI
-  transformer acts as an XSS-filter, neutralizing things like `javascript:`, `vbscript:` and `file:`
-  protocols. If you specify a custom function, this default filter won't be called, but you can
-  access it as `require('react-markdown').uriTransformer`. If you want to disable the default
-  transformer, pass `null` to this option.
-- `transformImageUri` - _function|null_ Function that gets called for each encountered image with a
-  single argument - `uri`. The returned value is used in place of the original.
-- `renderers` - _object_ An object where the keys represent the node type and the value is a React
-  component. The object is merged with the default renderers. The props passed to the component
-  varies based on the type of node.
-- `plugins` - _array_ An array of unified/remark parser plugins. If you need to pass options to the plugin, pass an array with two elements, the first being the plugin and the second being the options - for instance: `{plugins: [[require('remark-shortcodes'), {your: 'options'}]]`. (default: `[]`) Note that [not all plugins can be used](https://github.com/rexxars/react-markdown/issues/188#issuecomment-404710893).
-- `parserOptions` - _object_ An object containing options to pass to [remark-parse](https://github.com/remarkjs/remark/tree/master/packages/remark-parse)."
-          />
+          <ReactMarkdown source={lesson.description} />
         </MarkdownContainer>
         <ButtonContainer>
           <BackButton>
@@ -140,4 +111,16 @@ class Content extends Component {
   }
 }
 
-export default Content
+Content.propTypes = {
+  group: PropTypes.array,
+  lessons: PropTypes.array,
+  currentLesson: PropTypes.number,
+}
+
+const mapStateToProps = state => ({
+  group: state.lessonStore.group,
+  lessons: state.lessonStore.lessons,
+  currentLesson: state.lessonStore.currentLesson,
+})
+
+export default connect(mapStateToProps)(Content)
