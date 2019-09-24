b25de7af771502cd12b9661d8fcbd6825ec93bd8
Boat <boat46450@hotmail.com>
2019-09-24 20:42:55 +0700

    :sparkles: export markdown

--- src/features/common/index.js
+++ src/features/common/index.js
@@ -1,9 +1,6 @@
 import SideTab from './SideTab'
 import Tab from './Tab'
 import Nav from './Nav'
+import Markdown from './Markdown'
 
-export {
-  SideTab,
-  Tab,
-  Nav
-}
+export { SideTab, Tab, Nav, Markdown }
