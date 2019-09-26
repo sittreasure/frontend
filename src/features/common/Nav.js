1ac6d9b20c999abee64617ccabe64e3ac7c13de9
Boat <boat46450@hotmail.com>
2019-09-27 00:34:27 +0700

    :lipstick: set width 100%

--- src/features/common/Nav.js
+++ src/features/common/Nav.js
@@ -3,23 +3,22 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  display: flex;
   width: 100%;
-  height: 47px;
   margin-bottom: 9px;
 `
 
 const Title = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   width: auto;
-  height: 100%;
-  font-family: "ThaiSans Neue";
+  height: 47px;
+  font-family: 'ThaiSans Neue';
   font-style: normal;
   font-weight: bold;
   font-size: 21px;
   line-height: normal;
-  color: #C4C4C4;
+  color: #c4c4c4;
   background-color: #141414;
   padding: 0px 17px;
 `
