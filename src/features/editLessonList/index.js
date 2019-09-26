4aaa7c4fd79abc2f7820ee2c182366c4566b21c2
Boat <boat46450@hotmail.com>
2019-09-27 00:35:57 +0700

    :clown_face: mock edit lesson list page

---
+++ src/features/editLessonList/index.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { connect } from 'react-redux'
+import { Link as RouterLink } from 'react-router-dom'
+
+import { Nav } from '../common'
+import Card from './Card'
+import LessonActions from '../../redux/lessonStore'
+
+const Wrappper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  background-color: #1b1b1b;
+  overflow: hidden;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 0px 180px;
+`
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const Header = styled.h1`
+  font-family: 'ThaiSans Neue';
+  font-style: normal;
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 65px;
+  color: #c4c4c4;
+`
+
+const LessonContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  overflow: scroll;
+  overflow-y: auto;
+  max-height: 700px;
+`
+
+const Link = styled(RouterLink)`
+  text-decoration: none;
+`
+
+class EditLessonList extends Component {
+  componentDidMount() {
+    this.props.dispatch(LessonActions.getLessonGroup())
+    this.props.dispatch(LessonActions.getLesson())
+  }
+
+  render() {
+    return (
+      <Wrappper>
+        <Nav title="Edit Lesson" />
+        <Container>
+          <HeaderContainer>
+            <Header>Select Lesson</Header>
+          </HeaderContainer>
+          <LessonContainer>
+            {this.props.lessons.map((lesson, index) => {
+              const group = this.props.group.filter(group => {
+                return group.id === lesson.lessonGroup
+              })[0]
+              return (
+                <Link to={`/editLesson/${lesson.index}`} key={index}>
+                  <Card lesson={lesson} group={group} />
+                </Link>
+              )
+            })}
+          </LessonContainer>
+        </Container>
+      </Wrappper>
+    )
+  }
+}
+
+EditLessonList.propTypes = {
+  dispatch: PropTypes.func,
+  group: PropTypes.array,
+  lessons: PropTypes.array,
+}
+
+const mapStateToProps = state => ({
+  group: state.lessonStore.group,
+  lessons: state.lessonStore.lessons,
+})
+
+export default connect(mapStateToProps)(EditLessonList)
