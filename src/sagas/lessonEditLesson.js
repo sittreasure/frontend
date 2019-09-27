2bb86148bf42a21d98399cfb98a498565a176d9e
Boat <boat46450@hotmail.com>
2019-09-27 22:57:04 +0700

    :sparkles: create saga for edit lesson

---
+++ src/sagas/lessonEditLesson.js
@@ -0,0 +1,34 @@
+import { call, put, fork, takeEvery, select } from 'redux-saga/effects'
+
+import axios from '../libs/axios'
+import { accessToken } from '../utils'
+import LessonActions, { LessonTypes } from '../redux/lessonStore'
+
+function* editLesson({ id, data }) {
+  try {
+    const user = yield select(state => state.userStore.user)
+    yield call(async () => {
+      const body = {
+        description: data,
+        addedBy: user.id,
+      }
+      console.log('>>> [lessonEditLesson.js:15] data : ', data)
+      await axios.patch(`/mainapi/v1/lessons/${id}/`, body, {
+        headers: {
+          Authorization: `Bearer ${accessToken.getToken()}`,
+        },
+      })
+    })
+    yield put(LessonActions.getLesson())
+  } catch (error) {
+    console.log('>>> [lessonEditLesson.js:23] error : ', error)
+  }
+}
+
+function* editLessonListener() {
+  yield takeEvery(LessonTypes.EDIT_LESSON, editLesson)
+}
+
+export default function* editlessonsaga() {
+  yield fork(editLessonListener)
+}
