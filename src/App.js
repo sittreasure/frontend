6f6c418e967f8c0cf9f08f6dd9d0fce59b7ca6b8
Boat <boat46450@hotmail.com>
2019-10-17 20:30:10 +0700

    :sparkles: add route edit user

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './features'
 import Home from './features/home'
 import Playground from './features/playground'
 import Lesson from './features/lesson'
+import EditUser from './features/editUser'
 import EditLessonList from './features/editLessonList'
 import EditLesson from './features/editLesson'
 import Logout from './features/logout'
@@ -17,6 +18,7 @@ class App extends Component {
         <Route path="/home" exact component={Home} />
         <Route path="/playground" exact component={Playground} />
         <Route path="/lesson" exact component={Lesson} />
+        <Route path="/editUser" exact component={EditUser} />
         <Route path="/editLessonList" exact component={EditLessonList} />
         <Route path="/editLesson/:index" exact component={EditLesson} />
         <Route path="/logout" exact component={Logout} />
